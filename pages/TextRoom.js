1e82984ce41b9da88f60b843d09edc6e29738834
jepla20 <jepla20@student.sdu.dk>
2022-12-06 18:49:11 +0100

    added reading and displaying new messages from firestore

--- pages/TextRoom.js
+++ pages/TextRoom.js
@@ -1,47 +1,83 @@
 import { View, Text, StyleSheet, TextInput, Button, ScrollView, TouchableOpacity } from 'react-native';
-import React, {useRef, useState} from 'react';
-// import {firestore} from './firebase/firebase.js';
+import React, {useRef, useState, useEffect} from 'react';
 import firestore from '@react-native-firebase/firestore';
 import {addDoc, collection, serverTimestamp} from "@firebase/firestore";
 import { Form, FormItem } from 'react-native-form-component';
-
-// async function sendMessage(roomId, user, text){
-//   try{
-//     await addDoc(collection(firestore, 'chatRooms', roomId, 'messages'),{
-//       uid: user.uid,
-//       displayName: user.displayName,
-//       text: text,
-//       timestamp: serverTimestamp(),
-//     });
-//   } catch (error) {
-//     console.log(error);
-//     console.log('Her')
-//   }
-// }
+import { FlatList } from 'react-native';
 
 async function sendMessage(roomId, user, text){
   try{
     // await addDoc(collection(firestore, 'chatRooms', roomId, 'messages'), text);
     const ref = firestore().collection('chatRooms').doc(roomId).collection('messages');
-    // const timestamp = firestore.FieldValue.serverTimestamp()
     const timestamp = Date.now();
     await ref.add({
       uid: user.uid,
       displayName: user.displayName,
       text: text,
       timestamp: timestamp,
-    })
+    });
   } catch (error) {
     console.log(error);
     console.log('Her')
   }
 }
 
+function onResult(QuerySnapshot) {
+  console.log('Got Users collection result.');
+}
+
+function onError(error) {
+  console.error(error);
+}
+
+async function getMessages(roomId, callback){
+  try{
+    console.log('Here')
+    const ref = firestore().collection('chatRooms').doc(roomId).collection('messages');
+    const messages = ref.orderBy('timestamp', 'asc').get();
+    callback(messages)
+  } catch (error){
+    console.log(error);
+  }
+
+}
+
 export default function TextRoom({route, navigation}) {
   const {room} = route.params;
   const {user} = route.params;
   const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([])
   const messageRef = useRef();
+
+  // window.addEventListener('load', () => {
+  //   Fetchdata();
+  // });
+
+  useEffect(() => {
+    Fetchdata();
+  }, [room.id])
+
+  function onResult(QuerySnapshot) {
+    QuerySnapshot.docChanges().forEach(element => {
+      var data = element.doc.data();
+      setMessages(arr => [...arr, data]);
+    });
+    console.log('Got Mesages collection result.');
+  }
+  
+  function onError(error) {
+    console.error(error);
+  }
+
+  const Fetchdata = () => {
+    const ref = firestore().collection('chatRooms').doc(room.id).collection('messages');
+    ref.orderBy('timestamp', 'asc').onSnapshot(onResult, onError)    
+  }
+
+  // useEffect(() => {
+  //   getMessages(room.id, setMessages);
+  //   console.log(messages)
+  // }, [room.id])
 
   const handleSubmit = () => {
     sendMessage(room.id, user.user, message)
@@ -52,17 +88,22 @@ export default function TextRoom({route, navigation}) {
     <ScrollView contentContainerStyle={styles.container}>
       <Text style={styles.header}>Welcome to the {room.title} chat room!</Text>
       <ScrollView style={styles.textWindow}>
+        {/* {messages.map((message) => (
+          <View>
+            <Text>{message.text}</Text>
+          </View>
+        ))} */}
+        {
+          messages.map((data) => (
+            <Message
+            text={data.text}
+            name={data.displayName}
+            />
+          ))
+        }
+        
 
 
-
-        {/* <Form onButtonPress={handleSubmit} buttonText='Send'style={styles.form} buttonStyle={styles.button}>
-          <FormItem
-          value={message}
-          onChangeText={(message) => setMessage(message)}
-          ref={messageRef}
-          />
-
-        </Form> */}
       </ScrollView>
         <View style={styles.inputContainer}>
           <TextInput style={styles.form} placeholder='Enter Message' value={message} onChangeText={(message) => setMessage(message)} ref={messageRef}/>
@@ -71,7 +112,15 @@ export default function TextRoom({route, navigation}) {
           </TouchableOpacity>
         </View>
     </ScrollView>
-  )
+  );
+}
+
+const Message = ({text, name, }) => {
+  return(
+    <View>
+      <Text>{text} + {name}</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
